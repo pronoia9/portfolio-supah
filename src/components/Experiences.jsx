91823dd2f7b8e5984dc6243ea001a560f3fd887e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-02-26 19:09:26 +0300

    finish init experiences

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,5 +1,58 @@
 export const Experiences = () => {
-  return <div>Experiences</div>;
+  return (
+    <section>
+      <h2>Experience</h2>
+      <div className='cv-work'>
+        <div className='years'>nov 2018 - now</div>
+        <h3>
+          Fullstack &amp; Creative Developer @{' '}
+          <a href='https://www.studiogusto.com/' target='_blank'>
+            Studiogusto
+          </a>
+        </h3>
+      </div>
+      <div className='cv-work'>
+        <div className='years'>jan 2022 - now</div>
+        <h3>
+          Teacher - Webgl &amp; Creative Development @{' '}
+          <a href='https://www.nuovoistitutodesign.it/' target='_blank'>
+            NID - Nuovo Istituto Design
+          </a>
+        </h3>
+      </div>
+      <div className='cv-work'>
+        <div className='years'>oct 2021 - dec 2021</div>
+        <h3>
+          Senior Creative Developer @{' '}
+          <a href='https://www.akqa.com/' target='_blank'>
+            AKQA
+          </a>
+        </h3>
+      </div>
+      <div className='cv-work'>
+        <div className='years'>feb 2021</div>
+        <h3>
+          Teacher @{' '}
+          <a href='https://www.awwwards.com/academy/course/impress-everyone-with-a-3d-particle-scene-starting-from-bad-models' target='_blank'>
+            Awwwards Academy
+          </a>
+        </h3>
+      </div>
+      <div className='cv-work'>
+        <div className='years'>feb 2005 - oct 2018</div>
+        <h3>
+          Web Designer &amp; Developer @{' '}
+          <a href='https://www.websolute.com/' target='_blank'>
+            Websolute
+          </a>
+        </h3>
+      </div>
+      <div className='cv-work'>
+        <div className='years'>gen 2002 - feb 2005</div>
+        <h3>Freelance Web Designer &amp; Developer</h3>
+      </div>
+    </section>
+  );
 };
 
 export const Experience = () => {
