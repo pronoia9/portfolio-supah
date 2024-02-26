0fd111a48cf018a0262c7a0d5e0fcc5c8200a4cc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-02-26 19:09:13 +0300

    finish init app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import gsap from 'gsap';
+
+import { Works, Experiences, Showcases, Intro, Publications, Contacts } from 'components';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    gsap.to('section', {
+      opacity: 1,
+      delay: 3,
+      ease: 'power3.out',
+      duration: 2,
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Intro />
+      <Experiences />
+      <Showcases />
+      <Works />
+      <Publications />
+      <Contacts />
+      <br />
+      <br />
+      <br />
+      <section className='goodbye'>Thank you 👋</section>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
