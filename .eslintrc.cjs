654dcf4894f336347ab113ed43476efaf6993acf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-02-26 19:10:45 +0300

    disable no-unused-vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,6 @@ module.exports = {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/prop-types': 'off',
+    'no-unused-vars': 'off',
   },
 };
