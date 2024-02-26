96c358da1852b9e028500c881d3bf8dcaf8eccf0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-02-26 19:09:20 +0300

    finish init publications

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -1,5 +1,25 @@
 export const Publications = () => {
-  return <div>Publications</div>;
+  return (
+    <section>
+      <h2>Publications</h2>
+      <div className='cv-work'>
+        <div className='years'>Awwwards Course - feb 2021</div>
+        <h3>
+          <a href='https://www.awwwards.com/academy/course/impress-everyone-with-a-3d-particle-scene-starting-from-bad-models' target='_blank'>
+            Impress everyone with a 3D particle scene with Blender and Three.js
+          </a>
+        </h3>
+      </div>
+      <div className='cv-work'>
+        <div className='years'>Codrops Tutorial - apr 2023</div>
+        <h3>
+          <a href='https://tympanus.net/codrops/2023/04/27/building-a-webgl-carousel-with-react-three-fiber-and-gsap/' target='_blank'>
+            Building a WebGL Carousel with React Three Fiber and GSAP
+          </a>
+        </h3>
+      </div>
+    </section>
+  );
 };
 
 export const Publication = () => {
