f6c0f6c6efd6ed437a389dedbcbf586cabb59a59
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-02-26 19:09:33 +0300

    finish init works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,70 +1,22 @@
+import { showcase as data } from "utils/data";
+
 export const Works = () => {
-  return <div>Works</div>;
+  return (
+    <section>
+      <h2>Selected Experiments</h2>
+      <p>Some of my experiments from 2021 to 2024 which include glsl, svg, canvas, threejs and react.</p>
+      <div className='experiments'>
+        {data.map((exp, i) => {
+          const num = i + 1 < 10 ? `0${i + 1}` : i + 1;
+          return (
+            <div className='experiment' key={i.toString()}>
+              <Work image={exp.image} link={exp.link} name={`<em>${num}</em> ${exp.name}`} />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
-export const Work = () => {
-  return <div>Work</div>;
-};
-
-// import { useRef } from 'react';
-// import gsap from 'gsap';
-// import HTMLParse from 'react-html-parser';
-
-// const Work = ({ link, name, image }) => {
-//   const $root = useRef();
-//   const $overlay = useRef();
-//   const $link = useRef();
-
-//   const handleMouseEnter = (e) => {
-//     const bounds = $root.current.getBoundingClientRect();
-//     const top = e.clientY < bounds.y + bounds.height / 2;
-
-//     gsap.to($link.current, {
-//       x: '2rem',
-//       duration: 0.5,
-//       ease: 'power3.out',
-//     });
-//     gsap.fromTo(
-//       $overlay.current,
-//       {
-//         scaleY: 0,
-//         transformOrigin: top ? '0 0' : '0 100%',
-//       },
-//       {
-//         scale: 1,
-//         duration: 0.5,
-//         ease: 'power3.out',
-//       }
-//     );
-//   };
-
-//   const handleMouseLeave = (e) => {
-//     const bounds = $root.current.getBoundingClientRect();
-//     const top = e.clientY < bounds.y + bounds.height / 2;
-
-//     gsap.killTweensOf([$overlay.current, $link.current]);
-//     gsap.to($link.current, {
-//       x: 0,
-//       duration: 0.3,
-//       ease: 'power3.out',
-//     });
-//     gsap.to($overlay.current, {
-//       scaleY: 0,
-//       transformOrigin: top ? '0 0' : '0 100%',
-//       duration: 0.7,
-//       ease: 'power3.out',
-//     });
-//   };
-
-//   return (
-//     <a ref={$root} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href={link} target='_blank'>
-//       {image && <img src={image} alt={name} />}
-//       <span className='link' ref={$link}>
-//         {HTMLParse(name)}
-//       </span>
-//       <span className='overlay' ref={$overlay}></span>
-//     </a>
-//   );
-// };
-
-// export default Work;
+const Work = () => {};
