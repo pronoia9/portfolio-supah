09af1b44fb18670bbc12776d76a76b8829db1d4d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-02-28 19:12:06 +0300

    finish publication content

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -1,27 +1,27 @@
+import { publications as data } from 'utils/data';
+
 export const Publications = () => {
   return (
     <section>
       <h2>Publications</h2>
-      <div className='cv-work'>
-        <div className='years'>Awwwards Course - feb 2021</div>
-        <h3>
-          <a href='https://www.awwwards.com/academy/course/impress-everyone-with-a-3d-particle-scene-starting-from-bad-models' target='_blank'>
-            Impress everyone with a 3D particle scene with Blender and Three.js
-          </a>
-        </h3>
-      </div>
-      <div className='cv-work'>
-        <div className='years'>Codrops Tutorial - apr 2023</div>
-        <h3>
-          <a href='https://tympanus.net/codrops/2023/04/27/building-a-webgl-carousel-with-react-three-fiber-and-gsap/' target='_blank'>
-            Building a WebGL Carousel with React Three Fiber and GSAP
-          </a>
-        </h3>
-      </div>
+      {data.map((publication, i) => (
+        <Publication key={`publication-${i}`} {...publication} />
+      ))}
     </section>
   );
 };
 
-export const Publication = () => {
-  return <div>Publication</div>;
+export const Publication = ({ date, title, description, link }) => {
+  return (
+    <div className='cv-work'>
+      <div className='years'>
+        {title} - {date}
+      </div>
+      <h3>
+        <a href={link} target='_blank'>
+          {description}
+        </a>
+      </h3>
+    </div>
+  );
 };
