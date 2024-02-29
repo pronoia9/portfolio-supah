0bfd8b98770969d1e3dceb0136479b3fe9504e26
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-02-29 17:23:53 +0300

    move content to works data

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,10 +3,10 @@ import { showcase as data } from "utils/data";
 export const Works = () => {
   return (
     <section>
-      <h2>Selected Experiments</h2>
-      <p>Some of my experiments from 2021 to 2024 which include glsl, svg, canvas, threejs and react.</p>
+      <h2>{data.title}</h2>
+      <p>{data.subtitle}</p>
       <div className='experiments'>
-        {data.map((exp, i) => {
+        {data.data.map((exp, i) => {
           const num = i + 1 < 10 ? `0${i + 1}` : i + 1;
           return (
             <div className='experiment' key={`experiment-${i}`}>
