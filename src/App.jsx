360bc0d95e64a3a7b352277e7126833315507e9f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-03 18:14:23 +0300

    import themeprovider + globalstyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import gsap from 'gsap';
 
 import { Works, Experiences, Showcases, Intro, Publications, Contacts, Awards } from '/src/components';
+import { getTheme, systemThemeChangeHandler } from '/src/utils/utils';
+import GlobalStyles from '/src/style/GlobalStyles';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  // Event listener for system theme change
+  useEffect(() => {
+    const systemThemeWatcher = window.matchMedia('(prefers-color-scheme: dark)');
+    systemThemeWatcher.addEventListener('change', (e) => systemThemeChangeHandler(e, setTheme));
+    return () => { systemThemeWatcher.removeEventListener('change', systemThemeChangeHandler); };
+  }, []);
+
   useEffect(() => {
     gsap.to('section', {
       opacity: 1,
@@ -14,21 +26,23 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <Intro />
-      {/* <Experiences /> */}
-      {/* <Showcases /> */}
-      {/* <Works /> */}
-      {/* <Publications /> */}
-      {/* <Awards /> */}
-      {/* <Contacts /> */}
-      <br />
-      <br />
-      <br />
-      <section className='goodbye'>Thank you 👋</section>
-    </div>
+    <ThemeProvider theme={getTheme(theme)}>
+      <GlobalStyles />
+      <div>
+        <Intro />
+        {/* <Experiences /> */}
+        {/* <Showcases /> */}
+        {/* <Works /> */}
+        {/* <Publications /> */}
+        {/* <Awards /> */}
+        {/* <Contacts /> */}
+        <br />
+        <br />
+        <br />
+        <section className='goodbye'>Thank you 👋</section>
+      </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
-
