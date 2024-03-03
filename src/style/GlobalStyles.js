988db726c98005eff5659987ef7b0a77de5fab52
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-03 18:14:56 +0300

    add all styles to global styles (initially)

--- src/style/GlobalStyles.js
+++ src/style/GlobalStyles.js
@@ -24,6 +24,304 @@ const GlobalStyles = createGlobalStyle`
     --c-font: ${({ theme }) => theme.font};    
     --font: 'Matter';
   }
+
+  *,:after,:before {
+    box-sizing: border-box
+  }
+
+  html {
+    font-size: 10px
+  }
+
+  body {
+    background: #fff;
+    color: #000;
+    margin: 0;
+    padding: 0;
+    font-family: Matter,Helvetica,Arial,sans-serif;
+    font-size: 1.6rem;
+    font-weight: 300
+  }
+
+  #root {
+    padding: 4.4rem 5rem 6rem
+  }
+
+  @media(max-width: 440px) {
+    #root {
+      padding:4.4rem 2.5rem 6rem
+    }
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none
+  }
+
+  h2 {
+    font-size: 7.2rem;
+    font-weight: 500;
+    margin-bottom: 1rem
+  }
+
+  h2+p {
+    margin-bottom: 4rem
+  }
+
+  @media(max-width: 440px) {
+    h2 {
+      font-size:4rem
+    }
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    font-weight: 500;
+    margin: 0
+  }
+
+  .works .work {
+    font-size: 3rem;
+    font-weight: 500
+  }
+
+  @media(max-width: 440px) {
+    .works .work {
+      font-size:2rem
+    }
+  }
+
+  .works .work a {
+    background: #fff;
+    padding: 1rem 0;
+    display: block;
+    position: relative;
+    z-index: 1;
+    border-top: 1px solid #000;
+    border-bottom: 1px solid #000;
+    margin-top: -1px;
+    display: flex;
+    align-items: center
+  }
+
+  .works .work a:visited:after {
+    color: #000
+  }
+
+  .works .work a:after {
+    content: "âœ“";
+    position: absolute;
+    right: 10px;
+    color: #fff
+  }
+
+  @media(max-width: 440px) {
+    .works .work a:after {
+      -webkit-transform:none!important;
+      transform: none!important;
+      font-size: 1.5rem;
+      top: 15px;
+      right: 4px
+    }
+  }
+
+  .works .work img {
+    width: auto;
+    height: 80px;
+    margin-right: 10px
+  }
+
+  .works .work em {
+    font-weight: 300;
+    font-size: 2rem;
+    font-style: normal
+  }
+
+  .works .work span {
+    pointer-events: none
+  }
+
+  .link,.works .work span {
+    display: inline-block
+  }
+
+  @media(max-width: 440px) {
+    .link {
+      -webkit-transform:translateX(4px)!important;
+      transform: translateX(4px)!important
+    }
+  }
+
+  .overlay {
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    mix-blend-mode: difference;
+    -webkit-transform: scaleY(0);
+    transform: scaleY(0);
+    pointer-events: none
+  }
+
+  .experiments {
+    -webkit-columns: 2;
+    column-count: 2
+  }
+
+  @media(max-width: 640px) {
+    .experiments {
+      -webkit-columns:1;
+      column-count: 1
+    }
+  }
+
+  .experiments .experiment {
+    font-size: 1.8rem;
+    font-weight: 500
+  }
+
+  .experiments .experiment a {
+    background: #fff;
+    padding: 1rem 0;
+    display: block;
+    position: relative;
+    z-index: 1;
+    border-top: 1px solid #000;
+    border-bottom: 1px solid #000;
+    margin-top: -1px;
+    display: flex;
+    align-items: center
+  }
+
+  .experiments .experiment a img {
+    width: auto;
+    height: 60px;
+    margin-right: 10px
+  }
+
+  .experiments .experiment a:visited:after {
+    color: #000
+  }
+
+  .experiments .experiment a:after {
+    content: "âœ“";
+    position: absolute;
+    right: 10px;
+    color: #fff
+  }
+
+  .experiments .experiment em {
+    font-weight: 300;
+    font-size: 1.2rem;
+    font-style: normal;
+    display: inline-block;
+    width: 17px
+  }
+
+  .experiments .experiment span {
+    display: inline-block;
+    pointer-events: none
+  }
+
+  .intro,section {
+    opacity: 0
+  }
+
+  .intro {
+    margin-top: 6rem;
+    font-size: 2rem;
+    line-height: 1.3
+  }
+
+  .intro strong {
+    font-size: 3rem;
+    display: block;
+    margin-bottom: .5rem;
+    font-weight: 300
+  }
+
+  @media(max-width: 440px) {
+    .intro strong {
+      font-size:2.6rem
+    }
+  }
+
+  .cls-1,.cls-2 {
+    fill: none;
+    stroke: #000;
+    stroke-miterlimit: 10
+  }
+
+  .cls-1 {
+    stroke-width: 21px
+  }
+
+  .cls-2 {
+    stroke-width: 25px
+  }
+
+  .cls-3 {
+    fill: #fff
+  }
+
+  .cv-work {
+    display: block;
+    padding: 2rem 0;
+    font-size: 1.6rem;
+    border-bottom: 1px solid #000
+  }
+
+  .cv-work h3 {
+    font-size: 1.8rem
+  }
+
+  .cv-work .years {
+    font-size: 1.4rem;
+    margin-bottom: 3px
+  }
+
+  .cv-work a {
+    border-bottom: 1px dashed #000
+  }
+
+  ul {
+    margin: 0;
+    padding: 0
+  }
+
+  ul li {
+    list-style-type: none;
+    font-size: 1.6rem
+  }
+
+  ul li,ul li a {
+    padding: 1rem 0;
+    border-bottom: 1px solid #000
+  }
+
+  ul li a {
+    background: #fff;
+    display: block;
+    position: relative;
+    z-index: 1;
+    border-top: 1px solid #000;
+    margin-top: -1px
+  }
+
+  .no-border li {
+    border-bottom: none;
+    padding: 0
+  }
+
+  iframe {
+    display: none!important
+  }
+
+  /*# sourceMappingURL=main.8e896fda.chunk.css.map */
+
 `;
 
 export default GlobalStyles;
