a5de65fea9ef36537a7d4d21aadfb3ad456c196b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-10 19:33:29 +0300

    move work container + fix styling for showcase/work

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,8 @@
+import { useRef } from 'react';
+// import HTMLParse from 'react-html-parser';
 import styled from 'styled-components';
+import gsap from 'gsap';
 
-import { Showcase } from './Showcases';
 import { works as data } from '/src/utils/data';
 
 export const Works = () => {
@@ -13,9 +15,9 @@ export const Works = () => {
         {data.data.map((exp, i) => {
           const num = i + 1 < 10 ? `0${i + 1}` : i + 1;
           return (
-            <WorkContainer className='experiment' key={`experiment-${i}`}>
-              <Showcase name={`<em>${num}</em> ${exp.name}`} {...exp} />
-            </WorkContainer>
+            <WorksWrapper className='experiment' key={`experiment-${i}`}>
+              <Work name={`<em>${num}</em> ${exp.name}`} {...exp} />
+            </WorksWrapper>
           );
         })}
       </WorksContainer>
@@ -33,9 +35,10 @@ const WorksContainer = styled.div`
   }
 `;
 
-const WorkContainer = styled.div`
-  font-size: 1.8rem;
-  font-weight: 500;
+const WorksWrapper = styled.div`
+  &, a {
+    font-size: 1.8rem !important;
+  }
 
   a {
     background: #fff;
@@ -78,5 +81,125 @@ const WorkContainer = styled.div`
   span {
     display: inline-block;
     pointer-events: none;
+  }
+`;
+
+export const Work = ({ link, name, image, ...props }) => {
+  const $root = useRef(),
+    $overlay = useRef(),
+    $link = useRef();
+
+  const handleMouseEnter = (e) => {
+    const bounds = $root.current.getBoundingClientRect();
+    const top = e.clientY < bounds.y + bounds.height / 2;
+
+    gsap.to($link.current, {
+      x: '2rem',
+      duration: 0.5,
+      ease: 'power3.out',
+    });
+    gsap.fromTo(
+      $overlay.current,
+      {
+        scaleY: 0,
+        transformOrigin: top ? '0 0' : '0 100%',
+      },
+      {
+        scale: 1,
+        duration: 0.5,
+        ease: 'power3.out',
+      }
+    );
+  };
+
+  const handleMouseLeave = (e) => {
+    const bounds = $root.current.getBoundingClientRect();
+    const top = e.clientY < bounds.y + bounds.height / 2;
+
+    gsap.killTweensOf([$overlay.current, $link.current]);
+    gsap.to($link.current, {
+      x: 0,
+      duration: 0.3,
+      ease: 'power3.out',
+    });
+    gsap.to($overlay.current, {
+      scaleY: 0,
+      transformOrigin: top ? '0 0' : '0 100%',
+      duration: 0.7,
+      ease: 'power3.out',
+    });
+  };
+
+  return (
+    <WorkItem className='work' {...props}>
+      <a ref={$root} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href={link} target='_blank'>
+        {image && <img src={image} alt={name} />}
+        <span className='link' ref={$link}>
+          {name}
+          {/* {HTMLParse(name)} */}
+        </span>
+        <span className='overlay' ref={$overlay} />
+      </a>
+    </WorkItem>
+  );
+};
+
+const WorkItem = styled.div`
+  &, a {
+    font-size: 3rem;
+    font-weight: 500;
+
+    @media (max-width: 440px) {
+      font-size: 2rem;
+    }
+  }
+
+  a {
+    background: #fff;
+    padding: 1rem 0;
+    display: block;
+    position: relative;
+    z-index: 1;
+    border-top: 1px solid #000;
+    border-bottom: 1px solid #000;
+    margin-top: -1px;
+    display: flex;
+    align-items: center;
+
+    &:visited:after {
+      color: #000;
+    }
+
+    &:after {
+      content: '✓';
+      position: absolute;
+      right: 10px;
+      color: #fff;
+
+      @media (max-width: 440px) {
+        -webkit-transform: none !important;
+        transform: none !important;
+        font-size: 1.5rem;
+        top: 15px;
+        right: 4px;
+      }
+    }
+  }
+
+  img {
+    width: auto;
+    height: 80px;
+    margin-right: 10px;
+  }
+
+  em {
+    font-weight: 300;
+    font-size: 2rem;
+    font-style: normal;
+  }
+
+  span {
+    pointer-events: none;
+    display: inline-block;
   }
 `;
