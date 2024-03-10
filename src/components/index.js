dffeac491d082e8c287ac62e4aababbfaf650279
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-10 19:11:42 +0300

    import/export containers

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import { Awards } from './Awards';
 import { Contacts } from './Contacts';
+import { CvContainer, WorkContainer } from './Containers';
 import { Experiences } from './Experiences';
 import { Intro } from './Intro';
 import { Publications } from './Publications';
 import { Showcases } from './Showcases';
 import { Works } from './Works';
 
-export { Awards, Contacts, Experiences, Intro, Publications, Showcases, Works };
+export { Awards, Contacts, CvContainer, WorkContainer, Experiences, Intro, Publications, Showcases, Works };
