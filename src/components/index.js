dee5f00a83fbbd4e1705969a9ee2b713c706958f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-10 19:41:46 +0300

    delete styled component import/export

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import { Awards } from './Awards';
 import { Contacts } from './Contacts';
-import { Experiences, ExpContainer } from './Experiences';
+import { Experiences } from './Experiences';
 import { Intro } from './Intro';
 import { Publications } from './Publications';
 import { Showcases } from './Showcases';
 import { Works, Work } from './Works';
 
-export { Awards, Contacts, Experiences, ExpContainer, Intro, Publications, Showcases, Works, Work };
+export { Awards, Contacts, Experiences, Intro, Publications, Showcases, Works, Work };
