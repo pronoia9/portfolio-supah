f57ff204b9df453605f71b44410fcfb04e5af5bc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-10 19:42:09 +0300

    import/export experience

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import { Awards } from './Awards';
 import { Contacts } from './Contacts';
-import { Experiences } from './Experiences';
+import { Experiences, Experience } from './Experiences';
 import { Intro } from './Intro';
 import { Publications } from './Publications';
 import { Showcases } from './Showcases';
 import { Works, Work } from './Works';
 
-export { Awards, Contacts, Experiences, Intro, Publications, Showcases, Works, Work };
+export { Awards, Contacts, Experiences, Experience, Intro, Publications, Showcases, Works, Work };
