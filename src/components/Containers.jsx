7e2486332afd49bd17eb7691d58b6481b360dffb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-11 06:18:41 +0300

    change to animate func (fix overlay animation)

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import styled from 'styled-components';
-import { m } from 'framer-motion';
+import { m, animate } from 'framer-motion';
 
-import { sectionMotion, overlayMotion, linkMotion } from '/src/assets/motion';
+import { sectionMotion } from '/src/assets/motion';
 
 export const Section = ({ children, ...props }) => (
   <m.section {...sectionMotion()} {...props}>
@@ -53,80 +53,37 @@ export const ContactContainer = styled.li`
 `;
 
 export const OverlayContainer = ({ num, link, name, image, tags }) => {
-  const $root = useRef();
-  const [hover, setHover] = useState(null);
-  const [top, setTop] = useState(null);
+  const $root = useRef(), $overlay = useRef(), $link = useRef();
 
   const getTop = (e) => {
     const bounds = $root.current.getBoundingClientRect();
-    console.log('e.clientY < bounds.y   + bounds.height / 2');
-    console.log(`${e.clientY}       < ${bounds.y} + ${bounds.height}            / 2`);
     return e.clientY < bounds.y + bounds.height / 2;
   };
 
   const handleMouseEnter = (e) => {
-    setHover(true);
-    setTop(getTop(e));
+    // Animate the link
+    animate($link.current, { x: '2rem' }, { type: 'tween', duration: 0.5, ease: 'easeOut' });
+    // Animate the overlay
+    animate($overlay.current, { scaleY: 1, transformOrigin: getTop(e) ? 'top' : 'bottom' }, { type: 'tween', duration: 0.5, ease: 'easeOut' });
   };
 
   const handleMouseLeave = (e) => {
-    setHover(false);
-    setTop(getTop(e));
+    // Animate the link
+    animate($link.current, { x: 0 }, { type: 'tween', duration: 0.3, ease: 'easeOut' });
+    // Animate the overlay
+    animate($overlay.current, { scaleY: 0, transformOrigin: getTop(e) ? 'top' : 'bottom' }, { type: 'tween', duration: 0.7, ease: 'easeOut' });
   };
-
-  // useEffect(() => { }, [hover]);
-
-  //! DEBUGGING
-  useEffect(() => {
-    console.log(num, `${hover ? 'enter' : 'leave'} ${top ? 'top' : 'bottom'}`);
-  }, [hover]);
-
-  // IN
-  // gsap.to(
-  //   $link.current,
-  //   { x: '2rem', duration: 0.5, ease: 'power3.out' }
-  // );
-  // gsap.fromTo(
-  //   $overlay.current,
-  //   { scaleY: 0, transformOrigin: top ? '0 0' : '0 100%' },
-  //   { scale: 1, duration: 0.5, ease: 'power3.out' }
-  // );
-
-  // gsap.killTweensOf([$overlay.current, $link.current]);
-
-  // OUT
-  // gsap.to(
-  //   $link.current,
-  //   { x: 0, duration: 0.3, ease: 'power3.out' }
-  // );
-  // gsap.to(
-  //   $overlay.current,
-  //   { scaleY: 0, transformOrigin: top ? '0 0' : '0 100%', duration: 0.7, ease: 'power3.out' }
-  // );
 
   return (
     <OverlayLink ref={$root} href={link} target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       {image && <img src={image} alt={name} />}
 
-      <m.span className='link' {...linkMotion(hover)}>
+      <m.span ref={$link} className='link'>
         {num && <em>{num}</em>}
         {` ${name}`}
       </m.span>
 
-      <m.span
-        className='overlay'
-        initial={{ scaleY: 0, transformOrigin: top ? '0 0' : '0 100%' }}
-        animate={{
-          scaleY: hover ? 1 : 0,
-          transformOrigin: top ? '0 0' : '0 100%',
-          transition: {
-            type: 'tween',
-            scaleY: 0,
-            duration: hover ? 0.5 : 0.7,
-            ease: 'easeInOut',
-          },
-        }}
-      />
+      <m.span ref={$overlay} className='overlay' />
     </OverlayLink>
   );
 };
