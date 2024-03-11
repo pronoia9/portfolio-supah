75f8e073bb3f31388b9d9b579db627286ec3027c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-11 18:36:48 +0300

    delete styled components containers

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -1,5 +1,4 @@
 import { useRef } from 'react';
-import styled from 'styled-components';
 import { m, animate } from 'framer-motion';
 
 import { sectionMotion } from '/src/assets/motion';
@@ -9,48 +8,6 @@ export const Section = ({ children, ...props }) => (
     {children}
   </m.section>
 );
-
-export const ShowcaseContainer = styled.div`
-  a {
-    font-size: 3rem;
-    font-weight: 500;
-  }
-
-  img {
-    width: auto;
-    height: 80px;
-    margin-right: 10px;
-  }
-
-  em {
-    font-weight: 300;
-    font-size: 2rem;
-    font-style: normal;
-  }
-`;
-
-export const WorkContainer = styled(ShowcaseContainer)`
-  a {
-    font-size: 1.8rem;
-
-    img {
-      height: 60px;
-    }
-  }
-
-  em {
-    font-size: 1.2rem;
-    display: inline-block;
-    width: 17px;
-  }
-`;
-
-export const ContactContainer = styled.li`
-  a {
-    font-size: 1.6rem;
-    font-weight: 300;
-  }
-`;
 
 export const OverlayContainer = ({ num, link, name, image, tags }) => {
   const $root = useRef(), $overlay = useRef(), $link = useRef();
@@ -75,7 +32,7 @@ export const OverlayContainer = ({ num, link, name, image, tags }) => {
   };
 
   return (
-    <OverlayLink ref={$root} href={link} target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <a ref={$root} href={link} target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       {image && <img src={image} alt={name} />}
 
       <m.span ref={$link} className='link' transition={{ type: 'tween', ease: 'easeOut' }}>
@@ -84,70 +41,6 @@ export const OverlayContainer = ({ num, link, name, image, tags }) => {
       </m.span>
 
       <m.span ref={$overlay} className='overlay' transition={{ type: 'tween', ease: 'easeOut' }} />
-    </OverlayLink>
+    </a>
   );
 };
-
-const OverlayLink = styled.a`
-  background: var(--c-background);
-  padding: 1rem 0;
-  display: block;
-  position: relative;
-  z-index: 1;
-  border-top: 1px solid var(--c-font);
-  border-bottom: 1px solid var(--c-font);
-  margin-top: -1px;
-  display: flex;
-  align-items: center;
-
-  @media (max-width: 440px) {
-    font-size: 2rem;
-  }
-
-  &:visited:after {
-    color: var(--c-font);
-  }
-
-  &:after {
-    content: '✓';
-    position: absolute;
-    right: 10px;
-    color: var(--c-background);
-
-    @media (max-width: 440px) {
-      -webkit-transform: none !important;
-      transform: none !important;
-      font-size: 1.5rem;
-      top: 15px;
-      right: 4px;
-    }
-  }
-
-  span {
-    pointer-events: none;
-    display: inline-block;
-  }
-
-  .link {
-    display: inline-block;
-
-    @media (max-width: 440px) {
-      -webkit-transform: translateX(4px) !important;
-      transform: translateX(4px) !important;
-    }
-  }
-
-  .overlay {
-    position: absolute;
-    z-index: 2;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: var(--c-overlay);
-    mix-blend-mode: difference;
-    -webkit-transform: scaleY(0);
-    transform: scaleY(0);
-    pointer-events: none;
-  }
-`;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import { Awards } from './Awards';
 import { Contacts } from './Contacts';
-import { Section, OverlayContainer, ShowcaseContainer, WorkContainer, ContactContainer } from './Containers';
+import { Section, OverlayContainer } from './Containers';
 import { Experiences, Experience } from './Experiences';
 import { Intro } from './Intro';
 import { Publications } from './Publications';
@@ -12,9 +12,6 @@ export {
   Contacts,
   OverlayContainer,
   Section,
-  ShowcaseContainer,
-  WorkContainer,
-  ContactContainer,
   Experiences,
   Experience,
   Intro,
