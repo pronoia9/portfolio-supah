453366351b3772c533a5a40eca7e04c5e0265774
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-11 18:37:06 +0300

    removve styled components theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,25 @@
-import { useEffect, useState } from 'react';
-import { ThemeProvider } from 'styled-components';
 import { LazyMotion, domAnimation, m } from 'framer-motion';
 
 import { Works, Experiences, Showcases, Intro, Publications, Contacts, Awards } from '/src/components';
-import { GlobalStyles, themes } from '/src/assets/GlobalStyles';
 import { containerMotion } from '/src/assets/motion';
 
 function App() {
-  const [theme, setTheme] = useState(getSystemTheme);
-
-  // Event listener for system theme change
-  useEffect(() => {
-    const systemThemeWatcher = window.matchMedia('(prefers-color-scheme: dark)');
-    systemThemeWatcher.addEventListener('change', (e) => systemThemeChangeHandler(e, setTheme));
-    return () => { systemThemeWatcher.removeEventListener('change', systemThemeChangeHandler); };
-  }, []);
-
   return (
-    <ThemeProvider theme={getTheme(theme)}>
-      <GlobalStyles />
-      <LazyMotion features={domAnimation}>
-        <m.div {...containerMotion}>
-          <Intro />
-          <Experiences />
-          <Showcases />
-          <Works />
-          <Publications />
-          <Awards />
-          <Contacts />
-          <br />
-          <br />
-          <br />
-          <section className='goodbye'>Thanks Supah 👋</section>
-        </m.div>
-      </LazyMotion>
-    </ThemeProvider>
+    <LazyMotion features={domAnimation}>
+      <m.div {...containerMotion}>
+        <Intro />
+        <Experiences />
+        <Showcases />
+        <Works />
+        <Publications />
+        <Awards />
+        <Contacts />
+        <br />
+        <br />
+        <br />
+        <section className='goodbye'>Thanks Supah 👋</section>
+      </m.div>
+    </LazyMotion>
   );
 }
 export default App;
-
-const getSystemTheme = () => (window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-
-const systemThemeChangeHandler = (e, setTheme) => {
-  setTheme(e.matches ? 'dark' : 'light');
-};
-
-const getTheme = (theme) => (theme === 'light' ? themes.light : themes.dark);
